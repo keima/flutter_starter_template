eb48a722ccd4e3ed7f35aa86d957722f2b1197f9
Kouta Imanaka <keima@p-side.net>
2023-07-04 22:22:47 +0900

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_starter_template/main.dart';
+import 'package:flutter_starter_template/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
